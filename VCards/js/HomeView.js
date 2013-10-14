c4a65e8e5e0b7067518555577169b9d333fa09e5
Maria Popova <m.popova08@gmail.com>
2013-10-14 01:59:55 -0500

    Changes made by Maria Popova (m.popova08@gmail.com).

--- VCards/js/HomeView.js
+++ VCards/js/HomeView.js
@@ -1,7 +1,6 @@
 var HomeView = function(store) {
 
     this.initialize = function() {
-        // Define a div wrapper for the view. The div wrapper is used to attach events.
         this.el = $('<div/>');
         this.el.on('keyup', '.search-key', this.findByName);
     };
